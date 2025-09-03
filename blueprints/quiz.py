7b878cefb6f9a53a6f00723ade696b783e12168e
uyeong <uyeong1575@gmail.com>
2025-09-03 21:58:38 +0900

    검색기능 추가

--- blueprints/quiz.py
+++ blueprints/quiz.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from database import db
 
 # Quiz Blueprint 생성
@@ -6,10 +6,11 @@ quiz_bp = Blueprint('quiz', __name__)
 
 @quiz_bp.route("/list")
 def quiz_list():
-    """유저 목록 페이지 - DB에서 모든 유저의 quizInfo를 가져와서 카드로 표시"""
-    # DB에서 모든 유저의 quizInfo 데이터를 가져옵니다
-    users = list(db.users.find({}, {'quizInfo': 1, 'userId': 1}))
-    return render_template("quiz/list.html", users=users)
+   
+        users = list(db.users.find({}, {'quizInfo': 1, 'userId': 1}))
+    
+        return render_template("quiz/list.html", users=users )
+
 
 @quiz_bp.route("/dashboard")
 def quiz_dashboard():
